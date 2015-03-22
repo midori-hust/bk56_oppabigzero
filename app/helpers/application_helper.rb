bd807d428b7cde0b09d49e05e080e3152adbbdfd
midori-hust <phanthaobk@gmail.com>
2015-03-22 17:11:21 +0700

    #4445 Bootstrap,Layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+	#Return the full title on a per-page basis.
+	def full_title(page_title = "", basis_title = "")
+		 
+		if page_title.empty?
+			basis_title
+		else
+			"#{page_title} | #{basis_title}"
+		end	
+	end
+
 end
