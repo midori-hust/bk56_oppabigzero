1e814a67d214ebb4e3fa97e1d06a07e0b186aebe
midori-hust <phanthaobk@gmail.com>
2015-04-24 11:18:17 +0700

    Test Commit

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,2 @@
 module ApplicationHelper
-	#Return the full title on a per-page basis.
-	def full_title(page_title = "", basis_title = "")
-		 
-		if page_title.empty?
-			basis_title
-		else
-			"#{page_title} | #{basis_title}"
-		end	
-	end
-
 end
